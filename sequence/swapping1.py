9346d99ada21ecf2567a67622c0d055bfd851aa1
shrutichafle <chafleshruti4@gmail.com>
2025-02-24 07:04:19 +0000

    swapping of two number using two variable

---
+++ sequence/swapping1.py
@@ -0,0 +1,9 @@
+num1=int(input("enter first number :"))
+num2=int(input("enetr second number :"))
+print ("number before swapping")
+print("first number :",num1)
+print("second number :",num2)
+num1,num2=num2,num1
+print("number after swapping")
+print("first number :",num1)
+print("second number :",num2)
