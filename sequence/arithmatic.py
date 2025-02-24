b87ec4fc1d9ee6200bbd2887c37087d85297fa2f
shrutichafle <chafleshruti4@gmail.com>
2025-02-24 15:41:01 +0000

    add,mul,sub,div operation

---
+++ sequence/arithmatic.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number :"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print("Addition of two numbers is :",add)
+print("Subtraction of two numbers is :",sub)
+print("Multiplication of two numbers is :",mul)
+print("Division of two numbers is :",div)
