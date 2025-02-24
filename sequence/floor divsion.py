c1d2bcd0bf3470d7aec74e4a9422f93209db0dc8
shrutichafle <chafleshruti4@gmail.com>
2025-02-24 15:49:22 +0000

    divsion of two number using floor

---
+++ sequence/floor divsion.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+mod=num1%num2
+floor=num1//num2
+print("modulas: ",mod)
+print("floor division: ",floor_div)
