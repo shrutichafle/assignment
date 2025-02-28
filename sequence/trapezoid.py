5fe4bf13e95a4a64389a9af968652b75e329a702
shrutichafle <chafleshruti4@gmail.com>
2025-02-28 17:17:02 +0000

    area of trapezoid

---
+++ sequence/trapezoid.py
@@ -0,0 +1,5 @@
+base1=int(input("Enter the base1:"))
+base2=int(input("Enter the base2:"))
+height=int(input("Enter the height:"))
+area=1/2*(base1+base2)*height
+print("The area of trapezoid is:",area)
