2ab57a37e7a67a562cdfa73dd414c46c8ee35fbd
shrutichafle <chafleshruti4@gmail.com>
2025-03-02 10:04:32 +0000

    the number is nagavtive positive zero

---
+++ selection/positive nagavtive zero number.py
@@ -0,0 +1,7 @@
+number=int(input("enter a number:"))
+if number>0:
+    print("the number is positive")
+elif number<0:
+    print("the number is nagavtive")
+else:
+    print("the number is  zero")        
